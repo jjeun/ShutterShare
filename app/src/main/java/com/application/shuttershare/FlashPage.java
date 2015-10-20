4a6d76f3a41944437e536ce18cfb0753f332beeb
IsaacShella <isaac.shella.14@gmail.com>
2015-10-20 15:06:11 -0500

    Added line to java that should remove the navigation bar on the flashpage

--- app/src/main/java/com/application/shuttershare/FlashPage.java
+++ app/src/main/java/com/application/shuttershare/FlashPage.java
@@ -1,11 +1,12 @@
 package com.application.shuttershare;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 
 /*
@@ -22,6 +23,7 @@ public class FlashPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_flash_page);
 
             timer = new CountDownTimer(3000, 1000) {
